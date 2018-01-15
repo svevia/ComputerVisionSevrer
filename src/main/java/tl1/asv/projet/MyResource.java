5d5aa47b139d4465934cd1a59ef8be64adf77c0d
Pierre Bourgeois <dragonralph@gmail.com>
2018-01-15 19:15:15 +0100

    V2 of Project...

--- src/main/java/tl1/asv/projet/MyResource.java
+++ src/main/java/tl1/asv/projet/MyResource.java
@@ -19,15 +19,12 @@ import java.net.URLDecoder;
 import java.io.UnsupportedEncodingException;
 
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.bytedeco.javacpp.opencv_core.Mat;
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
 import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.json.JSONObject;
 
@@ -49,114 +46,212 @@ public class MyResource {
     public String getIt() {
         return "Got it!";
     }
-    
-    
-    
-	private static final String SERVER_UPLOAD_LOCATION_FOLDER = "/tmp/img";
 
-	/**
-	 * Upload a File
-	 */
 
-	@POST
+    private static final String SERVER_UPLOAD_LOCATION_FOLDER = "tmp/img";
+    private static final String SERVER_REFERENCES_FOLDER = "../datasets/processedRefs";
+
+    /**
+     * Upload a File
+     */
+
+    @POST
     @Consumes(MediaType.APPLICATION_JSON)
-	public Response uploadFile(String json) {
-	    System.out.println(json);
-		JSONObject obj = new JSONObject(json);
-		String img = obj.getString("img");
-		try {
-		    img = URLDecoder.decode(img, "UTF-8");
-		} catch (UnsupportedEncodingException e1) {
-		    // TODO Auto-generated catch block
-		    e1.printStackTrace();
-		}
-		String ext = "png";
-		String filePath = SERVER_UPLOAD_LOCATION_FOLDER	+ "/test/" + generateRandomInt() + "." + ext;
+    public Response uploadFile(String json) {
+        System.out.println(json);
+        JSONObject obj = new JSONObject(json);
+        String img = obj.getString("img");
+        try {
+            img = URLDecoder.decode(img, "UTF-8");
+        } catch (UnsupportedEncodingException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
+        String ext = "png";
+        String filePath = SERVER_UPLOAD_LOCATION_FOLDER + "/test/" + generateRandomInt() + "." + ext;
 
-		
-		byte[] data = Base64.getDecoder().decode(img);
-		try (OutputStream stream = new FileOutputStream(filePath)) {
-		    stream.write(data);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		String output = ChooseClass(filePath);
 
-		return Response.status(200).entity(output).build();
-	}
-	
-	public String ChooseClass(String filepath){
-		HashMap<String, Float> dists = new HashMap<String,Float>();
-		HashMap<String, Float> category = new HashMap<String,Float>();
+        byte[] data = Base64.getDecoder().decode(img);
+        try (OutputStream stream = new FileOutputStream(filePath)) {
+            stream.write(data);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-		Mat testMat = imread(filepath);
-		
-	
-		for(File f : listImgaes()){
-			if(!f.isDirectory()){
-				Mat matImg = imread(f.getAbsolutePath());
-				dists.put(f.getName(), recoImage.getDist(testMat, matImg));
-			}
-		}
-		
-		Map<String,Float> sorted = recoImage.sortByComparator(dists, true);
-		Set<Map.Entry<String, Float>> sortedSet = sorted.entrySet();
-		Iterator <Map.Entry<String, Float>> it = sortedSet.iterator();
-		int kMatch = 7;
-		for(int i = 0; i< kMatch;i++){
-			Entry<String,Float> item = it.next();
-			String cat = item.getKey().split("-")[0];
-			if(category.containsKey(cat)){
-				category.put(cat, category.get(cat) + 1);
-			}
-			else{
-				category.put(cat, (float) 1);
-			}
-		}
-		
-		Map<String,Float> sortedCat = recoImage.sortByComparator(category, false);
-		Set<Map.Entry<String, Float>> sortedSetCat = sortedCat.entrySet();
-		Iterator <Map.Entry<String, Float>> itCat = sortedSetCat.iterator();
-		
-		
-		return itCat.next().getKey();
-	}
-	
-	public File[] listImgaes(){
-		File dir = new File(SERVER_UPLOAD_LOCATION_FOLDER);
-		return dir.listFiles();
-	}
+        String output = ChooseClass(filePath);
 
-	// save uploaded file to a defined location on the server
-	private void saveFile(InputStream uploadedInputStream,
-			String serverLocation) {
+        return Response.status(200).entity(output).build();
+    }
 
-		try {
-			OutputStream outpuStream = new FileOutputStream(new File(serverLocation));
-			int read = 0;
-			byte[] bytes = new byte[1024];
 
-			outpuStream = new FileOutputStream(new File(serverLocation));
-			while ((read = uploadedInputStream.read(bytes)) != -1) {
-				outpuStream.write(bytes, 0, read);
-			}
-			outpuStream.flush();
-			outpuStream.close();
-		} catch (IOException e) {
+    @POST
+    @Path("/upload")
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    public Response uploadFile(
+            @FormDataParam("file") InputStream uploadedInputStream,
+            @FormDataParam("file") FormDataContentDisposition fileDetail) {
 
-			e.printStackTrace();
-		}
+        String uploadedFileLocation = SERVER_UPLOAD_LOCATION_FOLDER + "/" + fileDetail.getFileName();
 
-	}
-	
-	private int generateRandomInt(){
-		SecureRandom test = new SecureRandom();
-		int result = test.nextInt(1000000000);
-		return result;
-	}
+        // save it
+        saveFile(uploadedInputStream, uploadedFileLocation);
+
+        String output = "File uploaded to : " + uploadedFileLocation;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+    @GET
+    @Path("/analyse/{file}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response analyse(@PathParam("file") String file) {
+
+        String className = "NOK";
+
+        String filepath = SERVER_UPLOAD_LOCATION_FOLDER + "/" + file;
+
+        className = ChooseClass(filepath);
+
+
+
+        return Response.status(200).entity(className).build();
+    }
+
+
+    public String ChooseClass(String filepath) {
+        HashMap<String, Float> dists = new HashMap<>();
+        HashMap<String, Float> category = new HashMap<>();
+        HashMap<String, Float> categoryDistance = new HashMap<>();
+
+        Mat testMat = imread(filepath);
+
+
+        /**
+         * On teste les distances entre l'image de test et les images de références.
+         */
+        for (File f : listImages()) {
+            if (!f.isDirectory()) {
+                Mat matImg = imread(f.getAbsolutePath());
+                float resultMoy = recoImage.getDist(testMat, matImg);
+                dists.put(f.getName(), resultMoy);
+                System.out.println("Result for: "+f.getName()+"\t"+resultMoy);
+            }
+        }
+
+        /**
+         *
+         */
+        Map<String, Float> sorted = recoImage.sortByComparator(dists, true);
+        Set<Map.Entry<String, Float>> sortedSet = sorted.entrySet();
+
+
+        Iterator<Entry<String, Float>> testPrint = sortedSet.iterator();
+        while(testPrint.hasNext()){
+            Entry<String, Float> next = testPrint.next();
+            System.out.println("next: "+next.getKey()+":"+next.getValue());
+        }
+
+        Iterator<Map.Entry<String, Float>> it = sortedSet.iterator();
+        int kMatch = sortedSet.size();
+
+        // parcours des 7 premières je crois...
+        for (int i = 0; i < kMatch; i++) {
+            Entry<String, Float> item = it.next();
+            String cat = item.getKey().split("_")[0];
+            if (category.containsKey(cat)) {
+                category.put(cat, category.get(cat) + 1);
+
+                // add current distance
+                categoryDistance.put(cat, categoryDistance.get(cat) + item.getValue());
+            } else {
+                category.put(cat, (float) 1);
+                categoryDistance.put(cat, item.getValue());
+            }
+        }
+
+
+        // print all distances
+        for (String key :
+                categoryDistance.keySet()) {
+            float val = categoryDistance.get(key);
+
+            System.out.println("Original Value: "+ key+":"+val + " /" + category.get(key));
+            val /= category.get(key); // moyenne par le nombre d'entrée référencée
+            categoryDistance.put(key,val);
+            System.out.println("Final Value: "+ key+":"+val);
+
+
+        }
+
+
+        float lowest = -1;
+        String mostConfident = "NOK";
+        for (String marque :
+                categoryDistance.keySet()) {
+            if(lowest==-1 || lowest > categoryDistance.get(marque)){
+                lowest=categoryDistance.get(marque);
+                mostConfident=marque;
+            }
+        }
+
+
+
+        System.out.println("Most confident: "+ mostConfident);
+
+
+        return mostConfident;
+/*
+        Map<String, Float> sortedCat = recoImage.sortByComparator(category, false);
+        Set<Map.Entry<String, Float>> sortedSetCat = sortedCat.entrySet();
+        Iterator<Map.Entry<String, Float>> itCat = sortedSetCat.iterator();
+
+        System.out.println("SCORES");
+        Entry<String, Float> chosen = itCat.next();
+        String chosenClass= chosen.getKey();
+        System.out.println("Chosen with: " + chosenClass + "=>"+chosen.getValue());
+        while(itCat.hasNext()){
+            Entry<String, Float> next = itCat.next();
+            System.out.println(next.getKey() +"=>"+next.getValue());
+        }
+
+        return chosenClass;*/
+    }
+
+    public File[] listImages() {
+        File dir = new File(SERVER_REFERENCES_FOLDER);
+        return dir.listFiles();
+    }
+
+    // save uploaded file to a defined location on the server
+    private void saveFile(InputStream uploadedInputStream,
+                          String serverLocation) {
+
+        try {
+            OutputStream outputStream;
+            int read = 0;
+            byte[] bytes = new byte[1024];
+
+            outputStream = new FileOutputStream(new File(serverLocation));
+            while ((read = uploadedInputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+            outputStream.flush();
+            outputStream.close();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
+
+    private int generateRandomInt() {
+        SecureRandom test = new SecureRandom();
+        int result = test.nextInt(1000000000);
+        return result;
+    }
 }
--- src/main/java/tl1/asv/projet/recoImage.java
+++ src/main/java/tl1/asv/projet/recoImage.java
@@ -2,6 +2,8 @@ package tl1.asv.projet;
 
 import static org.bytedeco.javacpp.opencv_core.NORM_L2;
 import static org.bytedeco.javacpp.opencv_features2d.drawKeypoints;
+import static org.bytedeco.javacpp.opencv_highgui.*;
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
@@ -13,6 +15,7 @@ import java.util.Map.Entry;
 
 import org.bytedeco.javacpp.Loader;
 import org.bytedeco.javacpp.opencv_calib3d;
+import org.bytedeco.javacpp.opencv_imgproc;
 import org.bytedeco.javacpp.opencv_shape;
 import org.bytedeco.javacpp.opencv_core.DMatch;
 import org.bytedeco.javacpp.opencv_core.DMatchVector;
@@ -24,100 +27,125 @@ import org.bytedeco.javacpp.opencv_features2d.DrawMatchesFlags;
 import org.bytedeco.javacpp.opencv_xfeatures2d.SIFT;
 
 public class recoImage {
-	
-	static DMatchVector	selectBest(DMatchVector	matches,	int	numberToSelect)	{
-		DMatch[] sorted	= toArray(matches);	
-		Arrays.sort(sorted,	(a,	b)	->	{
-			return	a.lessThan(b)	?	-1	:	1;
-		});
-		DMatch[] best	= Arrays.copyOf(sorted,	numberToSelect);
-		return	new	DMatchVector(best);
-	}
-	
-	static DMatch[]	toArray(DMatchVector	matches)	{
-		assert	matches.size()	<=	Integer.MAX_VALUE;
-		int	n	=	(int)	matches.size();
-		//	Convert	keyPoints	to	Scala	sequence
-		DMatch[]	result	=	new	DMatch[n];
-		for	(int	i	=	0; i	<	n;	i++)	{
-			result[i]	=	new	DMatch(matches.get(i));
-		}
-		return	result;
-	}
-	
-	
-	static float getDist(Mat img1, Mat img2){
-		int nFeatures =	0;
-		int nOctaveLayers =	3;
-		double contrastThreshold =	0.03;
-		int edgeThreshold =	10;
-		double sigma =	1.6;
-		Loader.load(opencv_calib3d.class);
-		Loader.load(opencv_shape.class)	;
 
-		KeyPointVector kpv1 = new KeyPointVector();
-		KeyPointVector kpv2 = new KeyPointVector();
+    // Chargement statique des librairies pour optimisation
+    static {
+        Loader.load(opencv_calib3d.class);
+        Loader.load(opencv_shape.class);
+    }
 
-		SIFT	sift;
-		sift =	SIFT.create(nFeatures,	nOctaveLayers,	contrastThreshold,	edgeThreshold,	sigma);
-		sift.detect(img1, kpv1);
-		sift.detect(img2, kpv2);
-		
-		Mat feature1 = new Mat();
-		Mat feature2 = new Mat();
+    static DMatchVector selectBest(DMatchVector matches, int numberToSelect) {
+        DMatch[] sorted = toArray(matches);
+        Arrays.sort(sorted, (a, b) -> {
+            return a.lessThan(b) ? -1 : 1;
+        });
+        DMatch[] best = Arrays.copyOf(sorted, numberToSelect);
+        return new DMatchVector(best);
+    }
 
-		drawKeypoints(img1,	kpv1,	feature1,	new Scalar(255,	255,	255,	0),	DrawMatchesFlags.DRAW_RICH_KEYPOINTS);
-		drawKeypoints(img2,	kpv2,	feature2,	new Scalar(255,	255,	255,	0),	DrawMatchesFlags.DRAW_RICH_KEYPOINTS);
+    static DMatch[] toArray(DMatchVector matches) {
+        assert matches.size() <= Integer.MAX_VALUE;
+        int n = (int) matches.size();
+        //	Convert	keyPoints	to	Scalar	sequence
+        DMatch[] result = new DMatch[n];
+        for (int i = 0; i < n; i++) {
+            result[i] = new DMatch(matches.get(i));
+        }
+        return result;
+    }
 
-		Mat desc1 = new Mat();
-		Mat desc2 = new Mat();
-		
-		sift.compute(img1, kpv1, desc1);
-		sift.compute(img2, kpv2, desc2);
-		
-		BFMatcher matcher =	new BFMatcher(NORM_L2,	false);
-		DMatchVector match = new DMatchVector();
-		matcher.match(desc1,	desc2,	match);
-		
-		DMatchVector bestMatches = selectBest(match,25);
-		float moy = 0;
-		for(int j = 0; j < bestMatches.size();j++){
-			moy += bestMatches.get(j).distance();
-		}
-		moy = moy/bestMatches.size();
-		return moy;
-		
-	}
 
-	
-	
-    static Map<String, Float> sortByComparator(Map<String, Float> unsortMap, final boolean order)
-    {
+    /**
+     * Obtient la distance entre deux matrices d'image.
+     *
+     * @param img1
+     * @param img2
+     * @return
+     */
+    static float getDist(Mat img1_original, Mat img2_original) {
+        int nFeatures = 250;
+        int nOctaveLayers = 5;
+        double contrastThreshold = 0.03;
+        int edgeThreshold = 10;
+        double sigma = 1.6;
+
+        // BB conversion
+
+        Mat img1=new Mat(img1_original.size());
+        opencv_imgproc.cvtColor(img1_original,img1,opencv_imgproc.CV_BGR2GRAY);
+
+        Mat img2=new Mat(img2_original.size());
+        opencv_imgproc.cvtColor(img2_original,img2,opencv_imgproc.CV_BGR2GRAY);
+
+
+
+
+        KeyPointVector kpv1 = new KeyPointVector();
+        KeyPointVector kpv2 = new KeyPointVector();
+
+        SIFT sift;
+        sift = SIFT.create(nFeatures, nOctaveLayers, contrastThreshold, edgeThreshold, sigma);
+        sift.detect(img1, kpv1);
+        sift.detect(img2, kpv2);
+
+        Mat feature1 = new Mat();
+        Mat feature2 = new Mat();
+
+        Mat desc1 = new Mat();
+        Mat desc2 = new Mat();
+
+        sift.compute(img1, kpv1, desc1);
+        sift.compute(img2, kpv2, desc2);
+
+
+
+        drawKeypoints(img1, kpv1, feature1, new Scalar(255, 255, 255, 0), DrawMatchesFlags.DRAW_RICH_KEYPOINTS);
+        drawKeypoints(img2, kpv2, feature2, new Scalar(255, 255, 255, 0), DrawMatchesFlags.DRAW_RICH_KEYPOINTS);
+
+       // showImage(feature2,false);
+
+
+        BFMatcher matcher = new BFMatcher(NORM_L2, false);
+        DMatchVector match = new DMatchVector();
+        matcher.match(desc1, desc2, match);
+
+        /**
+         * On sélectionne les 25 meilleurs points.
+         */
+        DMatchVector bestMatches = selectBest(match, 25);
+        float moy = 0;
+        for (int j = 0; j < bestMatches.size(); j++) {
+            moy += bestMatches.get(j).distance();
+        }
+        moy = moy / bestMatches.size();
+        return moy;
+
+    }
+
+    private static void showImage( Mat img, boolean force) {
+        namedWindow("r", WINDOW_AUTOSIZE);    //	Create	a	window	for	display.
+        imshow("r", img);    //	Show	our	image	inside	it.
+        waitKey(0);    //	Wait	for	a	keystroke	in	the	window
+    }
+
+
+    static Map<String, Float> sortByComparator(Map<String, Float> unsortMap, final boolean order) {
 
         List<Entry<String, Float>> list = new LinkedList<Entry<String, Float>>(unsortMap.entrySet());
 
         // Sorting the list based on values
-        Collections.sort(list, new Comparator<Entry<String, Float>>()
-        {
-            public int compare(Entry<String, Float> o1,
-                    Entry<String, Float> o2)
-            {
-                if (order)
-                {
-                    return o1.getValue().compareTo(o2.getValue());
-                }
-                else
-                {
-                    return o2.getValue().compareTo(o1.getValue());
+        Collections.sort(list, (o1, o2) -> {
+            if (order) {
+                return o1.getValue().compareTo(o2.getValue());
+            } else {
+                return o2.getValue().compareTo(o1.getValue());
 
-                }
             }
         });
 
         // Maintaining insertion order with the help of LinkedList
         Map<String, Float> sortedMap = new LinkedHashMap<String, Float>();
-        for (Entry<String, Float> entry : list)
-        {
+        for (Entry<String, Float> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
         }
 

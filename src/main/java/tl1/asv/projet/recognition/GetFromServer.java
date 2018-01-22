4ff3e2abc7a9f753b72a77243d3cd964784cbac3
Pierre Bourgeois <dragonralph@gmail.com>
2018-01-22 16:20:08 +0100

    Update pom with google http client

---
+++ src/main/java/tl1/asv/projet/recognition/GetFromServer.java
@@ -0,0 +1,17 @@
+package tl1.asv.projet.recognition;
+
+public class GetFromServer {
+
+    public final String url = "http://www-rech.telecom-lille.fr/nonfreesift/";
+
+    public static void init() {
+
+       // HttpRequest
+       // HttpRequest
+
+
+
+    }
+
+
+}
